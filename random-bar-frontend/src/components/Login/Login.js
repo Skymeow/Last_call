20a1f2205589d136af55691e8b414b2e0d7ac2e4
Sky Xu <skyrocks88xmz@gmail.com>
2017-03-16 16:23:47 -0400

    clear commits

--- random-bar-frontend/src/components/Login/Login.js
+++ random-bar-frontend/src/components/Login/Login.js
@@ -25,20 +25,36 @@ class Login extends Component {
     this.setState(newState);
   }
 
+  componentDidMount(){
+    fetch(`http://localhost:3000/login`,{
+      method:"GET",
+      headers: {
+         "Authorization": window.localStorage.getItem("token")
+      }
+    })
+    .then((results)=>{
+      results.json().then((content)=>{
+        this.setState({user: content.user})
+      })
+    })
+    .catch((err)=>{
+      browserHistory.push("/:user_id/dashboard");
+    })
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-
-    fetch(`http://localhost:8000/users/login`, {
+     fetch(`http://localhost:8000/users/login`, {
       method: "POST",
       body: JSON.stringify({
-        user: this.state
+        user: this.state.user
       }),
       headers: {
         "Content-Type": "application/json"
       }
     })
     .then(() => {
-      
+
       browserHistory.push("/")
     })
     .catch((err) => {
@@ -53,7 +69,7 @@ class Login extends Component {
         <div className="container">
           <h2>Login Here</h2>
           <div className="form-container">
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)}>
               <h4>Email:</h4>
               <input name="email" type="email" onChange={this.handleChange.bind(this)} />
               <h4>Password:</h4>
